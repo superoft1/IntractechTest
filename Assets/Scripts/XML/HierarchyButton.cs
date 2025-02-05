9af3791cf52896c6e7d54f8e171c2620c35009db
Minh Dinh <55818127+superoft1@users.noreply.github.com>
2025-02-05 23:20:37 +0700

    * Update button behaviour

--- Assets/Scripts/XML/HierarchyButton.cs
+++ Assets/Scripts/XML/HierarchyButton.cs
@@ -1,16 +1,76 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HierarchyButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private HierarchyButton parentHierarchy = null;
+    private List<HierarchyButton> hierarchyChildren = new List<HierarchyButton>();
+    private RectTransform rect = null;
+
+    private void Awake() {
+        rect = this.transform as RectTransform;
+    }
+    
+    public void SetParent(HierarchyButton parent)
     {
-        
+        this.parentHierarchy = parent;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddChild(HierarchyButton child)
     {
-        
+        if (child != null && !hierarchyChildren.Contains(child))
+        {
+            child.SetParent(this);
+            hierarchyChildren.Add(child);
+        }
+    }
+
+    public void AddChildren(List<HierarchyButton> children)
+    {
+        if (children != null && children.Count > 0)
+        {
+            children.ForEach(child => {
+                AddChild(child);
+            });
+        }
+    }
+
+    private void ToggleContent(bool toggle)
+    {
+        if (hierarchyChildren != null)
+        {
+            hierarchyChildren.ForEach(child => 
+            {
+                child.ToggleButton(toggle);
+            });
+        }
+    }
+
+    public void ToggleButton(bool toggle)
+    {
+        this.gameObject.SetActive(toggle);
+        ToggleContent(toggle);
+        ForceUpdateLayout();
+    }
+
+    private void UpdateParent()
+    {
+        if (parentHierarchy)
+        {
+            parentHierarchy.ForceUpdateLayout();
+        }
+    }
+
+    public void ForceUpdateLayout()
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+        UpdateParent();
+    }
+
+    public void OnButtonClick()
+    {
+
     }
 }
